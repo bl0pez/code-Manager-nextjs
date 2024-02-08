6e2c7d979d1234f0e92e6681cd3898460120a338
bryan lopez <blopez4434@gmail.com>
2024-02-08 08:43:19 -0300

    Remove unused dependencies and add initial database schema

--- prisma/migrations/20240208113828_init/migration.sql
+++ prisma/migrations/20240208113828_init/migration.sql
@@ -2,24 +2,13 @@
 CREATE TYPE "Role" AS ENUM ('user', 'operator', 'admin');
 
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "fullName" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "isActive" BOOLEAN NOT NULL DEFAULT false,
-    "password" TEXT NOT NULL,
-    "role" "Role" NOT NULL DEFAULT 'user',
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "CodeBlue" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL,
     "team" TEXT NOT NULL,
     "location" TEXT NOT NULL,
     "operator" TEXT NOT NULL,
+    "officer" TEXT NOT NULL,
 
     CONSTRAINT "CodeBlue_pkey" PRIMARY KEY ("id")
 );
@@ -38,6 +27,18 @@ CREATE TABLE "Team" (
     "title" TEXT NOT NULL,
 
     CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
