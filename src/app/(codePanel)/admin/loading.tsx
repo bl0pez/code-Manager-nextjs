5aac52e74af38fb016fab760dcc4c6dc46fb92f3
bryan lopez <blopez4434@gmail.com>
2024-03-17 00:19:14 -0300

    Delete admin layout, loading, operators, and users pages

--- src/app/(codePanel)/admin/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="relative flex justify-center items-center h-96">
-      <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-indigo-500"></div>
-    </div>
-  );
-}
